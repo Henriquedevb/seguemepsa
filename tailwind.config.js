146a2a16ef7faaa5657d7020424e20e5d6cef0f6
Henrique Fontenele <henriquedevb@gmail.com>
2022-05-16 13:06:17 -0300

    feat: create configs postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  content: ["./src/**/*.{tsx, jsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  colors: {},
 };
